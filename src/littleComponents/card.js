ecc97896031e42f00f807d2ff29573a4f30129b8
Keengarthur <arthurkeengs@gmail.com>
2022-03-23 10:02:24 -0700

    Add files via upload

---
+++ src/littleComponents/card.js
@@ -0,0 +1,35 @@
+
+
+import RightCard from './cardRightComp'
+import  '../cssComponents/card.css'
+import image from '../image/9.jpg'
+import Button from './button'
+import Image from './image'
+import Color from './color'
+import {Button2} from './button'
+import LeftCard from './cardLeftComp'
+
+const CardComponent = (props) =>{
+    const time = new Date()
+    return <section className="cardComponentSection">
+        <RightCard 
+        description ={props.description}
+        />
+
+        <div className='cardComponentSectionSecond'>
+        <h4>Activity</h4>
+        <LeftCard name = 'Brian Cervieo' 
+        time = 'just now' 
+        place='@Brian Cervieo'/>
+        <LeftCard name = 'Brian Cervieo' 
+        place='@Brian Cervieo'/>
+        <LeftCard name = 'Brian Cervieo' 
+        time = 'just now' 
+        place='missing anything?'/>
+
+        </div>
+        
+    </section>
+}
+
+export default CardComponent
